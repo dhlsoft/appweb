b1959babced8d7634d8670d05d2be7aaf9ebe249
1846292@qq.com <dhldhl>
2017-09-25 13:45:37 +0800

    整体框架一版提交20170925

---
+++ web2py/applications/appweb/controllers/finance.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+def index():
+    return dict(message='')
+
+def myassets():
+    return dict(message='')
+
+def answer():
+    return dict(message='')
+
+def question():
+    return dict(message='')
+
+def inforPage():
+    return dict(message='')
+
+def mind():
+    return dict(message='')
+
+def configure():
+    return dict(message='')
